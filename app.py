0b8a55ad00f258b67cb21ee2eb17c0cb589b6ceb
elik09 <kelvinelik09@gmail.com>
2022-02-24 12:32:19 +0300

    Fixed Typo

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ users=[
 
 @app.route("/")
 def index():
-   return render_template('index.html',title="Home")
+   return render_template('index.html',title="Index")
 
 @app.route("/home")
 def home():
