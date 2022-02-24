dcf221257f6354175364a88512b343eae8abdbc2
elik09 <kelvinelik09@gmail.com>
2022-02-24 11:15:17 +0300

    Fixed Typo

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask
+
 app = Flask(__name__)
 
-
-
-# Main route
 @app.route("/")
-@app.route("/home")
 def index():
-	return "This the main main page"
-	# return render_template('index.html',title="Main Page")
+   return "Hello World"
 
-@app.route('/say_hello')
-def sayhello(): 
-    return "Hello There"
 
-@app.route('/say_hello/<username>')
-def sayhellotosomeone(username):
-    return f"Hello {username}"
-
-if (__name__ == "__main__"):
+if __name__ == '__main__':
     app.run(use_reloader=True,debug=True)
