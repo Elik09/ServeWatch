603ecfff92a09f8c576ff52e2eb3581a4d665f19
elik09 <kelvinelik09@gmail.com>
2022-02-25 15:13:09 +0300

    Initial commit

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def readme():
 def register():
 	form=RegistrationForm()
 	if form.validate_on_submit():
-		flash(f"<script>alert('Account created for {form.username.data}')</script>",'success')
+		flash(f'Account created for {form.username.data}!','success')
 		return redirect(url_for('home'))
 	return render_template('register.html',title="New Register",form=form)
 
