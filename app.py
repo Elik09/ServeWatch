08c4bec6bf4cc0e80fd7a448bf5785bd0aade745
elik09 <kelvinelik09@gmail.com>
2022-02-25 14:58:44 +0300

    Initial commit

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ def readme():
 @app.route('/register',methods=['GET','POST'])
 def register():
 	form=RegistrationForm()
-	if form.validate_on_submit()
+	if form.validate_on_submit():
+		flash(f"<script>alert('Account created')</script>")
 	return render_template('register.html',title="New Register",form=form)
 
 @app.route('/login')
