4520a1924abed0d2d669505efff77e6f453e4a58
elik09 <kelvinelik09@gmail.com>
2022-02-25 15:44:22 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for,Response,flash,redirect
+from flask import Flask,render_template,url_for,Response,flash,redirect,request
 
 from forms import RegistrationForm, LoginForm
 
