e23416c868248baf4de4d43c2f712edd1a3d3296
elik09 <kelvinelik09@gmail.com>
2022-02-25 15:15:40 +0300

    Initial commit

--- app.py
+++ app.py
@@ -44,8 +44,8 @@ def register():
 	form=RegistrationForm()
 	if form.validate_on_submit():
 		flash(f'Account created for {form.username.data}!','success')
-		# return redirect(url_for('home'))
-		return "Submitted"
+		return redirect(url_for('home'))
+
 	return render_template('register.html',title="New Register",form=form)
 
 @app.route('/login')
