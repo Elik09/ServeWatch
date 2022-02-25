36d77f9e2407450de09a3c0273c7f13be17e3c63
elik09 <kelvinelik09@gmail.com>
2022-02-25 11:07:20 +0300

    Fixed Typo

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-form wtforms.validators import DataRequired,Length
+from wtforms import StringField,PasswordField
+from wtforms.validators import DataRequired,Length,Email
 
 
 class RegistrationForm(FlaskForm):
