7e4455e7c6013b46cb3aba43e11b1b755967ba1c
elik09 <kelvinelik09@gmail.com>
2022-02-25 11:10:58 +0300

    Fixed Typo

--- forms.py
+++ forms.py
@@ -20,16 +20,12 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-	username=StringField('Username',
-		validators=[DataRequired(),Length(min=2,max=20)])
-
 	email=StringField('Email',
 		validators=[DataRequired(),Email()])
 
 	password=PasswordField('Password',
 		validators=[DataRequired()])
 
-	confirm_password=PasswordField('Confirm Password',
-		validators=[DataRequired(),EqualTo('password')])
+	
 
-	submit=SubmitField('Sign Up')
+	submit=SubmitField('Login')
