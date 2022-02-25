1428c1fa52415b88d16534850862b39a09634f46
elik09 <kelvinelik09@gmail.com>
2022-02-25 13:08:31 +0300

    Initial commit

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField
 import email_validator as Email
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length,Email,EqualTo
 
 
