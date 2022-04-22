e08e1bdee3582af707b32efd88459dd95a8e61bc
elik09 <kelvinelik09@gmail.com>
2022-04-22 11:50:51 +0300

    Removed sqlite file

--- project/api/views.py
+++ project/api/views.py
@@ -18,47 +18,51 @@ log_schema = PostSchema()
 # Configuration
 sender_address = 'servewatch@gmail.com'
 sender_pass = 'Servewatch2022!'
-receiver_address = 'tempmailctf@gmail.com'
+
+# Enter the emails to send to separated with a space
+receiver_addresses = 'plexeric40@gmail.com tempmailctf@gmail.com'
+
 mail_notifications="Enabled"
 
 
 def sendmessage(subject,mail_content):
-    global receiver_address,sender_address,sender_pass
+    global receiver_addresses,sender_address,sender_pass
     if mail_notifications != "Enabled":
         return False
-    print(f"[+] Sending email to {receiver_address}")
-    try:
-        #Setup the MIME
-        message = MIMEMultipart()
-        message['From'] = sender_address
-        message['To'] = receiver_address
-        if len(subject) > 2 :
-            message['Subject'] = f'{subject}'   #The subject line
-        else:
-            message['Subject'] = 'ServeWatch Notification'   #The subject line
-
-        if len(mail_content) < 2:
-            mail_content = "Hello,\nFile change detected on server. Please login to check activity.\nThank You"
-
-        #The body and the attachments for the mail
-        message.attach(MIMEText(mail_content, 'plain'))
-        #Create SMTP session for sending the mail
-        session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
-        session.starttls() #enable security
+    for receiver_address in receiver_addresses.split(" "):
+        print(f"[+] Sending email to {receiver_address}")
         try:
-            session.login(sender_address, sender_pass) #login with mail_id and password
-        except :
-            print("[-] An error occurred while logging in.Make sure the credentials are correct and check this page https://myaccount.google.com/lesssecureapps")
-            return False
+            #Setup the MIME
+            message = MIMEMultipart()
+            message['From'] = sender_address
+            message['To'] = receiver_address
+            if len(subject) > 2 :
+                message['Subject'] = f'{subject}'   #The subject line
+            else:
+                message['Subject'] = 'ServeWatch Notification'   #The subject line
 
-        text = message.as_string()
-        session.sendmail(sender_address, receiver_address, text)
-        session.quit()
-        print('[+] Mail Sent')
-        return True
-    except Exception as e :
-        print(f'[+] Email not sent {e}')
-        return False
+            if len(mail_content) < 2:
+                mail_content = "Hello,\nFile change detected on server. Please login to check activity.\nThank You"
+
+            #The body and the attachments for the mail
+            message.attach(MIMEText(mail_content, 'plain'))
+            #Create SMTP session for sending the mail
+            session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+            session.starttls() #enable security
+            try:
+                session.login(sender_address, sender_pass) #login with mail_id and password
+            except :
+                print("[-] An error occurred while logging in.Make sure the credentials are correct and check this page https://myaccount.google.com/lesssecureapps")
+                return False
+
+            text = message.as_string()
+            session.sendmail(sender_address, receiver_address, text)
+            session.quit()
+            print(f'[+] Mail Sent to {receiver_address}')
+            return True
+        except Exception as e :
+            print(f'[+] Email not sent to {receiver_address} {e}')
+            return False
 
 
 @api.route('/submit/logs', methods = ['POST'])
